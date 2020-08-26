cf426e5df07c287642f4f99579488ce4f1025dc2
yuzokamoto <yuzokamoto@gmail.com>
2020-08-26 17:19:02 -0300

    Adicionado o endpoint getAllPosts

--- src/controller/getAllPosts.ts
+++ src/controller/getAllPosts.ts
@@ -1,9 +1,12 @@
 import { Request, Response } from 'express'
 import BaseDB from '../database/BaseDatabase'
+import { PostBusiness } from '../business/PostBusiness'
 
 export async function getAllPosts(req: Request, res: Response): Promise<any> {
   try {
-    res.status(200).send({ message: `Get all posts success` })
+    const posts = await PostBusiness.getAllPosts();
+
+    res.status(200).send({ message: `Got all posts successfully`, posts })
   } catch (error) {
     res.status(400).send({ message: error.message, sqlMessage: error.sqlMessage })
   } finally {
